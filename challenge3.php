313def348ac1982428a0133dd7cab967df815e66
John Schwinghammer <hammer+git@insocada.com>
2014-01-16 11:44:53 -0600

    Added domain listing and prompt

--- challenge3.php
+++ challenge3.php
@@ -2,6 +2,7 @@
 
 require 'vendor/autoload.php';
 
+use OpenCloud\Rackspace;
 use OpenCloud\DNS;
 
 $credsFile = $_SERVER['HOME'] . "/.rackspace_cloud_credentials";
@@ -17,6 +18,36 @@ try {
         'username' => $cloudCreds['username'],
         'apiKey'   => $cloudCreds['api_key']
     ));
+
+    $cloudDNS = $client->DNSService();
+
+    echo "ID\tDomain\n--\t------\n";
+
+    $domCount = 0;
+    foreach ($cloudDNS->domainList() as $domain) {
+        $domCount++;
+        echo sprintf("%s\t%s\n", $domCount, $domain->Name());
+    }
+
+    // Get the number of servers to build
+    $validInput = FALSE;
+    while ($validInput == FALSE) {
+        echo "Please input a domain ID from the list above above to add an A record: ";
+        $handle = fopen ("php://stdin","r");
+        $numServers = trim(fgets($handle));
+        $filter_options = array(
+            'options' => array(
+                'min_range' => 1,
+                'max_range' => $domCount
+            )
+        );
+        if (filter_var($numServers, FILTER_VALIDATE_INT, $filter_options) == FALSE){
+            echo "Invalid input, please try again.\n\n";
+        } else {
+            $validInput = TRUE;
+            echo "\n";
+        }
+    }
 
 } catch (Exception $e) {
     die($e->getMessage());
