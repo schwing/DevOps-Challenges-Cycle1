d8734711ad5b8bf0d7e14546ada883784052425a
John Schwinghammer <hammer+git@insocada.com>
2014-01-20 15:02:16 -0600

    Challenge 4 started w/skeleton

---
+++ challenge4.php
@@ -0,0 +1,26 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use OpenCloud\Rackspace;
+use OpenCloud\DNS;
+
+$credsFile = $_SERVER['HOME'] . "/.rackspace_cloud_credentials";
+
+try {
+    // Try to open $credsFile and read the credentials from it
+    if (($cloudCreds = @parse_ini_file($credsFile)) == false) {
+        throw new Exception("Missing or unreadable INI file: " . $credsFile . "\n");
+    }
+
+    // Auth using credentials from $credsFile
+    $client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, array(
+        'username' => $cloudCreds['username'],
+        'apiKey'   => $cloudCreds['api_key']
+    ));
+
+} catch (Exception $e) {
+    die($e->getMessage());
+
+}
+?>
