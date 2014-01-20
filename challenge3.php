7cd09647f68a3a9096286db9c8e6302ad6d59260
John Schwinghammer <hammer+git@insocada.com>
2014-01-20 14:28:18 -0600

    Challenge 3 completed
    TODO:
        - Use readline
        - Check input

--- challenge3.php
+++ challenge3.php
@@ -26,6 +26,7 @@ try {
     $domCount = 0;
     foreach ($cloudDNS->domainList() as $domain) {
         $domCount++;
+        $domains[$domCount] = $domain;
         echo sprintf("%s\t%s\n", $domCount, $domain->Name());
     }
 
@@ -63,6 +64,58 @@ try {
             echo "\n";
         }
     }
+
+    // Get the TTL
+    $validInput = FALSE;
+    while ($validInput == FALSE) {
+        echo "Please input the TTL: ";
+        $handle = fopen ("php://stdin","r");
+        $ttl = trim(fgets($handle));
+        if (filter_var($ttl, FILTER_VALIDATE_INT) == FALSE) {
+            echo "Invalid input, please try again.\n\n";
+        } else {
+            $validInput = TRUE;
+            echo "\n";
+        }
+    }
+
+    // Get the IP address
+    $validInput = FALSE;
+    while ($validInput == FALSE) {
+        echo "Please input the IPv4 address: ";
+        $handle = fopen ("php://stdin","r");
+        $ipAddr = trim(fgets($handle));
+        if ($ipAddr == NULL) {
+            echo "No fqdn provided. Please try again.\n\n";
+        } else {
+            $validInput = TRUE;
+            echo "\n";
+        }
+    }
+
+    // Add the A record
+    $record = $domains[$domId]->record();
+    $response = $record->create(array(
+        'type' => 'A',
+        'name' => $fqdn,
+        'ttl' => $ttl,
+        'data' => $ipAddr
+    ));
+
+    // Wait for the update to complete
+    $callback = function($response) {
+        printf("%s %s\n", $response->Status(), $response->Name());
+        if ($response->Status() == 'ERROR') {
+            throw new Exception(sprintf("\tError code [%d] message [%s]\n\tDetails: %s\n", $response->error->code, $response->error->message, $response->error->details));
+        }
+        else if ($response->Status() == 'COMPLETED') {
+            // Only adding 1 record, so only one should be returned
+            $records = $response->response->records[0];
+            echo sprintf("Record added successfully: %s\nTTL: %s\nIP Address: %s\n", $records->name, $records->ttl, $records->data);
+        }
+    };
+
+    $response->waitFor("COMPLETED", 300, $callback);
 
 } catch (Exception $e) {
     die($e->getMessage());
