fadb75770d337de21d33b1311f9fb0f4e513e234
John Schwinghammer <hammer+git@insocada.com>
2014-01-22 11:09:01 -0600

    Implemented checks for existing container

--- challenge4.php
+++ challenge4.php
@@ -21,6 +21,8 @@ try {
     $cloudFiles = $client->objectStoreService('cloudFiles', 'DFW');
 
     // Get the new container name
+    // TODO: constraints on input from (https://github.com/rackspace/php-opencloud/blob/master/docs/userguide/ObjectStore/Storage/Container.md):
+    //  "Container names must be valid strings between 0 and 256 characters. Forward slashes are not currently permitted."
     $validInput = FALSE;
     while ($validInput == FALSE) {
         echo "Please input a name for the new container: ";
@@ -35,6 +37,17 @@ try {
     }
 
     // Try to create the container
+    $createResponse = $cloudFiles->createContainer($containerName);
+
+    // Throw an exception if we weren't able to create the container.
+    if ($createResponse == FALSE) {
+        // Try to get the container. If this fails for any reason, it will throw an exception.
+        // Otherwise, the container exists because we were able to get it, so we throw our own custom exception and exit.
+        $cloudFiles->getContainer($containerName);
+        throw new Exception(sprintf("A container named \"%s\" already exists.\nPlease run this script again with a different container name.\n", $containerName));
+    }
+
+    
 
 } catch (Exception $e) {
     die($e->getMessage());
