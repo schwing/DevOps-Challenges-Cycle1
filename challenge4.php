1883a1e70348a18cebb8490dca8f099af3bcaad3
John Schwinghammer <hammer+git@insocada.com>
2014-01-22 10:05:41 -0600

    Prompt for new container name

--- challenge4.php
+++ challenge4.php
@@ -3,7 +3,6 @@
 require 'vendor/autoload.php';
 
 use OpenCloud\Rackspace;
-use OpenCloud\DNS;
 
 $credsFile = $_SERVER['HOME'] . "/.rackspace_cloud_credentials";
 
@@ -18,6 +17,24 @@ try {
         'username' => $cloudCreds['username'],
         'apiKey'   => $cloudCreds['api_key']
     ));
+
+    $cloudFiles = $client->objectStoreService('cloudFiles', 'DFW');
+
+    // Get the new container name
+    $validInput = FALSE;
+    while ($validInput == FALSE) {
+        echo "Please input a name for the new container: ";
+        $handle = fopen ("php://stdin","r");
+        $containerName = trim(fgets($handle));
+        if ($containerName == NULL) {
+            echo "Invalid input, please try again.\n\n";
+        } else {
+            $validInput = TRUE;
+            echo "\n";
+        }
+    }
+
+    // Try to create the container
 
 } catch (Exception $e) {
     die($e->getMessage());
